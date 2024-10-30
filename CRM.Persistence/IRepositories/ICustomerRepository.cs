1df50dd0e1d8dacca7898a647160d6ff84315fb8
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-30 21:42:17 +0000

    added persistence layer

---
+++ CRM.Persistence/IRepositories/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using CRM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Persistence.IRepositories
+{
+
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> GetByIdAsync(Guid id);
+        Task AddAsync(Customer customer);
+        void Update(Customer customer);
+        void Delete(Customer customer);
+        Task<bool> SaveChangesAsync();
+    }
+
+}
---
+++ CRM.Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,49 @@
+﻿using CRM.Domain.Entities;
+using CRM.Persistence.DbContexts;
+using CRM.Persistence.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Persistence.Repositories
+{
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CRMRelationalContext _context;
+
+        public CustomerRepository(CRMRelationalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetByIdAsync(Guid id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            await _context.Customers.AddAsync(customer);
+        }
+
+        public void Update(Customer customer)
+        {
+            _context.Customers.Update(customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+
+}
