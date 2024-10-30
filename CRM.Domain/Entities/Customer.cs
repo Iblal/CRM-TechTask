57cdf5b3f6e0079f723c4b1280893bc8d9d2e5d5
iblalahmed12 <iblalahmed12@gmail.com>
2024-10-30 21:42:03 +0000

    Added domain layer

---
+++ CRM.Domain/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace CRM.Domain.Entities
+{
+    public class Customer
+    {
+        public Customer(string name, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public void Update(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
---
+++ CRM.Persistence/Data/CRMRelationalContext.cs
@@ -0,0 +1,15 @@
+﻿using CRM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Persistence.DbContexts
+{
+    public class CRMRelationalContext : DbContext
+    {
+        public CRMRelationalContext (DbContextOptions<CRMRelationalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = default!;
+    }
+}
